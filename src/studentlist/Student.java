f38911381945a2e6fa6cc8290b842daaab8add18
shkurtimateo <59849650+shkurtimateo@users.noreply.github.com>
2020-01-20 15:47:47 -0500

    Update Student.java

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -1,22 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package studentlist;
 
-/**
- *
- * @author srinivsi
- */
 public class Student
 {
     private String name;
     private int age;
 
-    /**
-     * @return the name
-     */
     public String getName() {
         return name;
     }
@@ -35,9 +24,6 @@ public class Student
         return age;
     }
 
-    /**
-     * @param age the age to set
-     */
     public void setAge(int age) {
         this.age = age;
     }
